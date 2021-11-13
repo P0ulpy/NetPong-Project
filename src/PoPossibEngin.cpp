b41b9d524c18c2fa52ee589368abd6b9d8b907be
Krakhazblong <julen.leremboure@laposte.net>
2021-11-14 00:55:41 +0100

    Creating the PoPossibEngin

--- src/PoPossibEngin.cpp
+++ src/PoPossibEngin.cpp
@@ -1,1 +1,89 @@
-#include "PoPossibleEngin.h"
+#include "PoPossibEngin.h"
+
+#include "Game.h"
+
+
+//--- Constructors - Destructors ---
+PoPossibEngin::PoPossibEngin()
+{
+	initWindow();
+}
+
+PoPossibEngin::~PoPossibEngin()
+{
+
+}
+
+//--- Initializers ---
+void PoPossibEngin::initWindow()
+{
+	_videoMode = sf::VideoMode(1200, 900);
+
+	_window = std::make_unique<sf::RenderWindow>(_videoMode, "Net-Pong Project", sf::Style::Close | sf::Style::Titlebar);
+	_window->setFramerateLimit(60);
+}
+
+//--- Update - Render ---
+void PoPossibEngin::update()
+{
+	pollEvents();
+}
+
+void PoPossibEngin::updateDeltaTime()
+{
+	_deltaTime = _dtClock.restart().asSeconds();
+}
+
+void PoPossibEngin::render()
+{
+	_window->clear();
+
+	_window->display();
+}
+
+void PoPossibEngin::run()
+{
+	while (isRunning())
+	{
+		updateDeltaTime();
+		update();
+		render();
+	}
+}
+
+//--- Methods ---
+void PoPossibEngin::pollEvents()
+{
+	while (_window->pollEvent(_sfmlEvent))
+	{
+		switch (_sfmlEvent.type)
+		{
+		case sf::Event::Closed:
+			_window->close();
+			break;
+		case sf::Event::KeyPressed:
+			handleKeyPressed(_sfmlEvent.key.code);
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void PoPossibEngin::handleKeyPressed(const sf::Keyboard::Key& keyPressed) const
+{
+	switch (keyPressed)
+	{
+		case sf::Keyboard::Escape:
+			_window->close();
+			break;
+		default:
+			break;
+	}
+}
+
+//--- Getters - Setters ---
+bool PoPossibEngin::isRunning() const
+{
+	return _window->isOpen();
+}
--- src/PoPossibEngin.h
+++ src/PoPossibEngin.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "SFML/Graphics.hpp"
+
+class PoPossibEngin
+{
+	//Window and SFML stuff
+	sf::VideoMode _videoMode;
+	std::unique_ptr<sf::RenderWindow> _window;
+	sf::Event _sfmlEvent;
+
+	//Time - DeltaTime
+	sf::Clock _dtClock;
+	float _deltaTime;
+
+	//Initializers
+	void initWindow();
+
+public:
+	//Constructors - Destructors
+	PoPossibEngin();
+	virtual ~PoPossibEngin();
+
+	//Accessors
+	bool isRunning() const;
+
+	//Functions
+	void pollEvents();
+	void handleKeyPressed(const sf::Keyboard::Key& keyPressed) const;
+
+	void update();
+	void updateDeltaTime();
+	void render();
+	void run();
+};
